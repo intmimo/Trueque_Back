688e9c35fc67682bc3755262e932b821412ad5d9
intmimito01 <noemiarango6@gmail.com>
2025-07-15 01:54:26 -0600

    Modificacion index motrar todos los productos

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,7 +10,10 @@ class ProductController extends Controller
     public function index()
     {
         $products = Product::all();
-        return view('products.index', compact('products'));
+        return response()->json([
+            'message' => 'Lista de productos',
+            'data' => $products,
+        ]);
     }
 
     public function create()
